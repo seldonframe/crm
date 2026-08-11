826fc0d6bf8637e21957a1f07d55a5f257d7fa59
Maxime Houle <maximehoule100@gmail.com>
2026-08-11 07:06:54 -0400

    chore: align demo repository links

--- packages/crm/src/lib/demo/constants.ts
+++ packages/crm/src/lib/demo/constants.ts
@@ -1,9 +1,9 @@
-export const DEMO_REPO_URL = "https://github.com/seldonframe/crm";
+export const DEMO_REPO_URL = "https://github.com/seldonframe/seldonframe";
 
 export const DEMO_DEPLOY_URL =
-  "https://vercel.com/new/clone?repository-url=https://github.com/seldonframe/crm&env=DATABASE_URL,NEXTAUTH_SECRET&envDescription=Set%20up%20your%20Neon%20database%20and%20auth%20secret&envLink=https://github.com/seldonframe/crm#environment-variables";
+  "https://vercel.com/new/clone?repository-url=https://github.com/seldonframe/seldonframe&env=DATABASE_URL,NEXTAUTH_SECRET&envDescription=Set%20up%20your%20Neon%20database%20and%20auth%20secret&envLink=https://github.com/seldonframe/seldonframe#environment-variables";
 
-export const DEMO_CLOUD_WAITLIST_URL = "https://github.com/seldonframe/crm/discussions";
+export const DEMO_CLOUD_WAITLIST_URL = "https://github.com/seldonframe/seldonframe/discussions";
 
 export const DEMO_BLOCK_MESSAGE = "This is a live demo. Fork SeldonFrame to build your own.";
 
